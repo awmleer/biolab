55f3331d8845e4ca0fbd573bd71b9158c9023f67
awmleer <awmleer@sparker.xyz>
2017-05-03 20:28:00 +0800

    reagent detail

--- src/pages/reagent-detail/reagent-detail.ts
+++ src/pages/reagent-detail/reagent-detail.ts
@@ -2,16 +2,14 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-reagent-detail',
-  templateUrl: 'reagent-detail.html',
+    selector: 'page-reagent-detail',
+    templateUrl: 'reagent-detail.html',
 })
 export class ReagentDetailPage {
 
-  constructor(
-      public navCtrl: NavController,
-      public navParams: NavParams
-  ) {}
-
-  ionViewDidLoad() {}
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams
+    ) {}
 
 }

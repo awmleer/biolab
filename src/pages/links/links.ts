2a65946cfdc5e1565fedb728fb66d8e972d3798d
awmleer <awmleer@sparker.xyz>
2017-07-18 15:13:41 +0800

    [feat] links page

--- src/pages/links/links.ts
+++ src/pages/links/links.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {InAppBrowser} from "@ionic-native/in-app-browser";
 
 @Component({
   selector: 'page-links',
@@ -7,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+      public navCtrl: NavController,
+      private inAppBrowser: InAppBrowser
+  ) {}
 
+  openLink(link:string){
+      this.inAppBrowser.create(link,'_blank',{
+          zoom:'no'
+      });
   }
 
 }

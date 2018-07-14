0ff9a2d850295dda337bd2aac4f1827cfef90035
awmleer <awmleer@sparker.xyz>
2018-07-14 22:45:28 +0800

    instrument detail page

--- src/pages/instrument-detail/instrument-detail.module.ts
+++ src/pages/instrument-detail/instrument-detail.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { InstrumentDetailPage } from './instrument-detail';
+import {PipesModule} from '../../pipes/pipes.module'
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { InstrumentDetailPage } from './instrument-detail';
   ],
   imports: [
     IonicPageModule.forChild(InstrumentDetailPage),
+    PipesModule,
   ],
 })
 export class InstrumentDetailPageModule {}
--- src/pages/instrument-detail/instrument-detail.ts
+++ src/pages/instrument-detail/instrument-detail.ts
@@ -1,25 +1,38 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import {InstrumentDetail} from "../../classes/instrument";
+import {InstrumentService} from "../../services/instrument.service";
+import {ToastService} from "../../services/toast.service";
+import {PhotoViewer} from "@ionic-native/photo-viewer";
 
-/**
- * Generated class for the InstrumentDetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-instrument-detail',
   templateUrl: 'instrument-detail.html',
 })
 export class InstrumentDetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  instrument:InstrumentDetail;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private instrumentService: InstrumentService,
+    private toastService: ToastService,
+    private photoViewer: PhotoViewer,
+  ) {}
+
+  ionViewWillLoad(){
+    this.instrumentService.getInstrumentDetail(this.navParams.get('instrumentId')).then(data=>{
+      this.instrument=data;
+    }).catch(() => {
+      this.toastService.toast('获取仪器信息失败');
+      this.navCtrl.pop();
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad InstrumentDetailPage');
+  viewPicture(url,title){
+    this.photoViewer.show(url,title);
   }
+
 
 }

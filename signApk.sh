a9fb474a369f65b106e326d20987abac99c655cb
awmleer <awmleer@sparker.xyz>
2018-12-04 09:40:33 +0800

    fix sign apk script

--- signApk.sh
+++ signApk.sh
@@ -1,1 +1,1 @@
-jarsigner -verbose -keystore bioLab.keystore -signedjar bioLab.apk platforms/android/build/outputs/apk/android-release-unsigned.apk bioLab.keystore
+jarsigner -verbose -keystore bioLab.keystore -signedjar bioLab.apk platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk bioLab.keystore
